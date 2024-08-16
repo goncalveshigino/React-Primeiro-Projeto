39ab38914486867474ab605c47b59e7c6e1e6c78
goncalveshigino <goncalveshigino20@gmail.com>
2024-08-16 10:48:24 +0100

    feat:Navegacao entre paginas usando Link em components e useNavegate em funcoes

--- src/App/routes/index.tsx
+++ src/App/routes/index.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes as Switch, Navigate } from "react-router-dom";
 
-import { Dashboard } from "../pages"
+import { Dashboard, Login } from "../pages"
 
 export const Routes = () => {
 
     return(
         <BrowserRouter>
            <Switch>
-                <Route path="/pagina-inicial" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Dashboard />} />
+                
 
-                <Route path="*" element={<Navigate to="pagina-inicial" />} />
+                <Route path="*" element={<Navigate to="home" />} />
            </Switch>
         </BrowserRouter>
     )
