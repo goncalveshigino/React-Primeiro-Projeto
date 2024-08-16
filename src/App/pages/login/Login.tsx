3202c64ade5e2f315fa7c94d7d7ef55f540cd893
goncalveshigino <goncalveshigino20@gmail.com>
2024-08-16 11:08:18 +0100

    feat:Adicionei o formulario

--- src/App/pages/login/Login.tsx
+++ src/App/pages/login/Login.tsx
@@ -1,6 +1,7 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+//import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+/*export const Login = () => {
     const navigate = useNavigate();
 
   const handleClick = () => {
@@ -10,7 +11,41 @@ export const Login = () => {
   return (
      <div>
         Login
+        
         <button onClick={handleClick}>Página Incial</button>
+     </div>
+  );
+}*/
+
+export const Login = () => {
+ 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
+  const handleLogin = () => {
+     console.log(email)
+     console.log(password)
+  }
+
+  return (
+     <div>
+        <form>
+
+           <label>
+              <span>Email</span>
+              <input value={email} onChange={e => setEmail(e.target.value)}/>
+           </label>
+
+           <label>
+              <span>Senha</span>
+              <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+           </label>
+
+           <button type="button" onClick={handleLogin}>
+               Entrar
+           </button>
+
+        </form>
      </div>
   );
 }
